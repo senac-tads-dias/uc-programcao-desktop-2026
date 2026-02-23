f3ba50b4c9b9c482ac9f97a3049c44bd35e5263d
Jeandro Dias <jeandro57803707@edu.ms.senac.br>
2026-02-23 19:30:43 -0400

    Calculadora com uma unica função

--- aula_2002/calc_grid.py
+++ aula_2002/calc_grid.py
@@ -1,64 +1,50 @@
 import tkinter as tk
 
-def somar():    
+def calcular(op):    
     try:
+        
         n1 = float(entrada1.get())
         n2 = float(entrada2.get())
         
-        soma = n1 + n2 
-        labelResult.config(text=f"Resultado: {soma}")
-    except:
-        labelResult.config(text=f"Informe numeros validos!!!")
-def sub(self):    
-    try:
-        n1 = float(entrada1.get())
-        n2 = float(entrada2.get())
-        
-        soma = n1 - n2 
-        labelResult.config(text=f"Resultado: {soma}")
+        resultado = 0.0
+        if op == "+":
+            resultado = n1 + n2
+        if op == "-":
+            resultado = n1 - n2
+        if op == "*":
+            resultado = n1 * n2
+        if op == "/":
+            resultado = n1 / n2
+
+        labelResult.config(text=f"Resultado: {resultado}")
     except:
         labelResult.config(text=f"Informe numeros validos!!!")
 
-def mult(self):    
-    try:
-        n1 = float(entrada1.get())
-        n2 = float(entrada2.get())
-        
-        soma = n1 * n2 
-        labelResult.config(text=f"Resultado: {soma}")
-    except:
-        labelResult.config(text=f"Informe numeros validos!!!")
-def div(self):    
-    try:
-        n1 = float(entrada1.get())
-        n2 = float(entrada2.get())
-        
-        soma = n1 / n2 
-        labelResult.config(text=f"Resultado: {soma}")
-    except:
-        labelResult.config(text=f"Informe numeros validos!!!")
 
 #criando Janela
 janela = tk.Tk()
 janela.title("Calculadora")
-janela.geometry("300x300")
+janela.geometry("400x300")
 
-labelNum1 = tk.Label(janela, text="Numero 1:").grid(row=0, column=0)
-entrada1 = tk.Entry(janela).grid(row=0, column=1)
+labelNum1 = tk.Label(janela, text="Numero 1:").grid(row=0, column=0, padx=10, pady=5, sticky="w" )
+entrada1 = tk.Entry(janela)
+entrada1.grid(row=0, column=1, padx=10, pady=5)
 
-labelNum2 = tk.Label(janela, text="Numero 2:").grid(row=1, column=0)
-entrada2 = tk.Entry(janela).grid(row=1, column=1)
+labelNum2 = tk.Label(janela, text="Numero 2:").grid(row=1, column=0, padx=10, pady=5, sticky="w")
+entrada2 = tk.Entry(janela)
+entrada2.grid(row=1, column=1, padx=10, pady=5)
 
-labelResult = tk.Label(janela, text="Resultado: ").grid(row=2, column=2)
+botao = tk.Button(janela, text="Somar", command=lambda:calcular("+"))
+botao.grid(row=2, column=0, padx=10, pady=5)
+botao1 = tk.Button(janela, text="Subtrair", command=lambda:calcular("-"))
+botao1.grid(row=2, column=1, padx=10, pady=5)
+botao2 = tk.Button(janela, text="Mult", command=lambda:calcular("*"))
+botao2.grid(row=2, column=2, padx=10, pady=5)
+botao3 = tk.Button(janela, text="Dividir", command=lambda:calcular("/"))
+botao3.grid(row=2, column=3, padx=10, pady=5)
 
-
-botao = tk.Button(janela, text="Somar", command=somar).grid(row=3, column=0)
-botao = tk.Button(janela, text="Subtrair", command=sub).grid(row=3, column=1)
-botao = tk.Button(janela, text="Mult", command=mult).grid(row=3, column=2)
-botao = tk.Button(janela, text="Dividir", command=div).grid(row=3, column=3)
-
-
-
+labelResult = tk.Label(janela, text="Resultado: ")
+labelResult.grid(row=3, column=3)
 
 
 janela.mainloop()
